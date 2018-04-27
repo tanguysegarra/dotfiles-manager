dcd00a3cb1168ec69970cf320ce774b704d43d76
gnathl <tanguy.segarra@epita.fr>
2018-04-27 13:00:53 +0200

    added dotfiles and wallpaper

--- gpdots.sh
+++ gpdots.sh
@@ -4,7 +4,7 @@ git add -f .zshrc
 git add -f .vimrc
 git add -f .i3block.conf
 git add -f .config/i3/config
-git add -f Pictures/dog.jpg
+git add -f Pictures/mario-avengers.jpg
 git add -f README.md
 git add -f gpdots.sh
 git commit -m 'added dotfiles and wallpaper'
